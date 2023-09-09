ea099bfa81c476bdd21cbf7335378bd3d081d6ac
tainasilvak <tainacunha42@gmail.com>
2023-09-09 18:09:33 -0300

    finalizei meu primeiro projeto node

--- index.js
+++ index.js
@@ -12,13 +12,42 @@ app.get('/users', (request, response) => {
 
 app.post('/users', (request, response) => {
     const { name, age } = request.body // recebendo as informações do body
-
     const user = { id: uuid.v4(), name, age } // criando meu usuário
 
     users.push(user) // usuário adicionado ao array users
 
     return response.json(user)
+})
 
+app.put('/users/:id', (request, response) => {
+    const {id} = request.params // pegar o id do user
+    const {name, age} = request.body // pegar as informações que quero atualizar
+
+    const updateUser = {id, name, age} // usuário atualizado
+
+    const index = users.findIndex( user => user.id === id) // encontrar a posição que o usuário está dentro do array
+
+    if(index < 0) {
+        return response.status(404).json({message: "User not found"}) // usuário não encontrado
+    }
+
+    users[index] = updateUser // atualizo o usuário
+
+    return response.json(updateUser) // mostra na tela o usuário atualizado
+})
+
+app.delete('/users/:id', (request, response) => {
+    const {id} = request.params
+
+    const index = users.findIndex(user => user.id === id)
+    
+    if(index < 0) {
+        return response.status(404).json({message: "User not found"}) // usuário não encontrado
+    }
+
+    users.splice(index, 1)
+
+    return response.status(204).json()
 })
 
 app.listen(port, () => {
